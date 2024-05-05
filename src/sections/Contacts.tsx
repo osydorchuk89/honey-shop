4db53ca81c4f28813ae6a9b7113bdbf131f74963
Oleksii Sydorchuk <oleksii.sydorchuk@gmail.com>
2024-05-05 10:30:10 +0200

    fixed another background image

--- src/sections/Contacts.tsx
+++ src/sections/Contacts.tsx
@@ -3,7 +3,7 @@ import { SocialMediaBox } from "../components/SocialMediaBox";
 
 export const Contacts = () => {
     return (
-        <div className="bg-[url('honeycombs.png')] bg-cover shadow-[inset_0_0_0_2000px_rgba(235,165,38,0.8)] lg:px-20 sm:px-10 px-4 lg:py-[104px] py-20 flex flex-col items-center lg:gap-16 gap-10">
+        <div className="bg-[url('/honeycombs.png')] bg-no-repeat bg-cover bg-center shadow-[inset_0_0_0_2000px_rgba(235,165,38,0.8)] lg:px-20 sm:px-10 px-4 lg:py-[104px] py-20 flex flex-col items-center lg:gap-16 gap-10">
             <SectionTitle
                 text="In each sip is nature and care"
                 fontColor="black"
