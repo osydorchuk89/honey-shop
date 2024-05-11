022224836620326dfad87f23a249a994fa3e5430
Oleksii Sydorchuk <oleksii.sydorchuk@gmail.com>
2024-05-11 18:03:09 +0200

    small fix

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -54,9 +54,9 @@ export const ContactForm = ({
         resolver: zodResolver(ContactFormSchema),
     });
 
-    const EMAILJS_SERVICE_ID = process.env.VITE_EMAILJS_SERVICE_ID!;
-    const EMAILJS_TEMPLATE_ID = process.env.VITE_EMAILJS_TEMPLATE_ID!;
-    const EMAILJS_PUBLIC_KEY = process.env.VITE_EMAILJS_PUBLIC_KEY!;
+    const EMAILJS_SERVICE_ID = process.env.EMAILJS_SERVICE_ID!;
+    const EMAILJS_TEMPLATE_ID = process.env.EMAILJS_TEMPLATE_ID!;
+    const EMAILJS_PUBLIC_KEY = process.env.EMAILJS_PUBLIC_KEY!;
     console.log(EMAILJS_SERVICE_ID);
     console.log(EMAILJS_PUBLIC_KEY);
 
